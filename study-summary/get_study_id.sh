eeb6a654297b309c3e080f94689a376cbb949d57
bcorrie <bdcorrie@gmail.com>
2021-08-25 20:43:44 +0000

    Use python3

--- study-summary/get_study_id.sh
+++ study-summary/get_study_id.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl -k -s --data '{"fields":["study.study_id"]}' $1 | python -m json.tool | grep study_id |  awk '{$1=$1;print}' | sort -u | awk -F '"' 'BEGIN {print "study_id"}{print $4}'
+curl -k -s --data '{"fields":["study.study_id"]}' $1 | python3 -m json.tool | grep study_id |  awk '{$1=$1;print}' | sort -u | awk -F '"' 'BEGIN {print "study_id"}{print $4}'
