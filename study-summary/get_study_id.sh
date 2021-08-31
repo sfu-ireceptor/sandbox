801a625378946ee1b89c4ebb3bdce75f62c10c6c
bcorrie <bdcorrie@gmail.com>
2021-08-31 21:06:25 +0000

    Added some error checking for command line args.

--- study-summary/get_study_id.sh
+++ study-summary/get_study_id.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
+
+SCRIPT_DIR=`dirname "$0"`
+
+# check number of arguments
+NB_ARGS=1
+if [ $# -ne $NB_ARGS ];
+then
+    echo "$0: wrong number of arguments ($# instead of $NB_ARGS)"
+    echo "usage: $0 URL"
+    exit 1
+fi
+
+
 curl -k -s --data '{"fields":["study.study_id"]}' $1 | python3 -m json.tool | grep study_id |  awk '{$1=$1;print}' | sort -u | awk -F '"' 'BEGIN {print "study_id"}{print $4}'
