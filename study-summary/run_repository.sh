e37aca4d2d2ef56fc8add7f92e1d8878f0fbaf6f
bcorrie <bdcorrie@gmail.com>
2021-08-31 21:03:34 +0000

    Generalized to use generic functionality and support multiple output types.

--- study-summary/run_repository.sh
+++ study-summary/run_repository.sh
@@ -1,9 +1,31 @@
 #!/bin/bash
 
-echo "Running study summary for $1"
+SCRIPT_DIR=`dirname "$0"`
 
-./get_study_id.sh $1 > /tmp/studies.tsv
+# check number of arguments
+NB_ARGS=3
+if [ $# -lt $NB_ARGS ];
+then
+    echo "$0: wrong number of arguments ($# instead of $NB_ARGS)"
+    echo "usage: $0 REPERTOIRE_URL AIRR_FIELD QUERY_FILE [--json_output|--html_output|--irplus_output]"
+    exit 1
+fi
 
-python3 ./ADC-study-summary.py $1 /tmp/studies.tsv study_id 
+REPERTOIRE_URL=$1
+AIRR_FIELD=$2
+QUERY_FILE=$3
+
+if [ $# -eq 3 ];
+then
+	OUTPUT_TYPE="--json_output"
+else
+	OUTPUT_TYPE=$4
+fi
+
+echo "Running study summary for ${REPERTOIRE_URL}"
+
+./get_repertoire_field.sh ${REPERTOIRE_URL} ${AIRR_FIELD} ${QUERY_FILE} > /tmp/studies.tsv
+
+python3 ./ADC-study-summary.py ${REPERTOIRE_URL} /tmp/studies.tsv ${AIRR_FIELD} ${OUTPUT_TYPE}
 
 
