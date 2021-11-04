763178098a4ec2b0049b6c480b9818a9c1ab230c
bcorrie <bdcorrie@gmail.com>
2021-11-04 23:37:39 +0000

    First truly working version. Needs cleanup, few more features.

--- adc-search/adc-search.py
+++ adc-search/adc-search.py
@@ -1,4 +1,4 @@
-import urllib.request, urllib.parse
+import requests
 import pandas as pd
 import numpy as np
 import argparse
@@ -8,58 +8,81 @@ import sys
 import time
 import yaml
 
-def processQuery(query_url, header_dict, query_json="{}", verbose=False):
-    # Build the required JSON data for the post request. The user
-    # of the function provides both the header and the query data
+def getField(dictionary, field_path, verbose):
+    field_list = field_path.split(".")
+    current_object = dictionary
+    for field_name in field_list:
+        if field_name in current_object:
+            current_object = current_object[field_name]
+            if isinstance(current_object, list):
+                if verbose:
+                    print("Warning: Processing array field, first object only", file=sys.stderr)
+                current_object = current_object[0]
 
-    # Encode the JSON for the HTTP requqest
-    query_json_encoded = query_json.encode('utf-8')
-
-    # Try to connect the URL and get a response. On error return an
-    # empty JSON array.
-    try:
-        # Build the request
-        request = urllib.request.Request(query_url, query_json_encoded, header_dict)
-        # Make the request and get a handle for the response.
-        response = urllib.request.urlopen(request)
-        # Read the response
-        url_response = response.read()
-        # If we have a charset for the response, decode using it, otherwise assume utf-8
-        if not response.headers.get_content_charset() is None:
-            url_response = url_response.decode(response.headers.get_content_charset())
+            current_field = field_name
         else:
-            url_response = url_response.decode("utf-8")
-    except urllib.error.HTTPError as e:
-        print('ERROR: Server could not fullfil the request to ' + query_url)
-        print('ERROR: Error code = ' + str(e.code) + ', Message = ', e.read())
-        return json.loads('[]')
-    except urllib.error.URLError as e:
-        print('ERROR: Failed to reach the server')
-        print('ERROR: Reason =', e.reason)
-        return json.loads('[]')
-    except Exception as e:
-        print('ERROR: Unable to process response')
-        print('ERROR: Reason =' + str(e))
-        return json.loads('[]')
+            return None, None
 
+    return current_field, current_object
+
+def performRearrangementQuery(rearrangement_url, repertoires, rearrangement_dict, repertoire_field_df, output_handle, service_delay, verbose):
+
+    count = 0
+    total = len(repertoires)
+    print("Info: Processing %d repertoires from %s" %(total,rearrangement_url))
+    print("{\n\"repository\":\"%s\",\n\"results\":["%(rearrangement_url), file=output_handle)
+    t_start = time.perf_counter()
+    for repertoire in repertoires:
+        repertoire_id = repertoire['repertoire_id']
+        query_dict = generateRearrangementQuery(repertoire_id, rearrangement_dict)
+        query_response = processQuery(rearrangement_url, query_dict, verbose)
+        # Print out an error if the query failed.
+        if query_response == None:
+            print('ERROR: Query %s failed to %s'%(query_dict, rearrangement_url))
+            continue
+
+        count = count + 1
+
+        repertoire_info = dict()
+        for index, row in repertoire_field_df.iterrows():
+            field, value = getField(repertoire, row[0], verbose)
+            if not field == None:
+                repertoire_info[row[0]] = value
+                #print("\"%s\":\"%s\","%(field, value), file=output_handle)
+        query_response["Repertoire"] = repertoire_info
+        print("%s"%(json.dumps(query_response, indent = 4)), file=output_handle)
+        if count < total:
+            print(",", file=output_handle)
+        time.sleep(service_delay)
+
+    
+    print("]\n}", file=output_handle)
+    t_end = time.perf_counter()
+    print("Info: Performed %d queries in %f s, %f queries/s" %
+          (count, t_end - t_start, count/(t_end - t_start)))
+
+def processQuery(query_url, query_dict, verbose):
+
+    # Do a post request
+    url_response = requests.post(query_url, json=query_dict)
+
+    # Get the JSON data as a dictionary.
     try:
-        json_data = json.loads(url_response)
+        json_data = url_response.json()
     except json.decoder.JSONDecodeError as error:
-        if force:
-            print("WARNING: Unable to process JSON response: " + str(error))
-            if verbose:
-                print("Warning: URL response = " + url_response)
-            return json.loads('[]')
-        else:
-            print("ERROR: Unable to process JSON response: " + str(error))
-            if verbose:
-                print("ERROR: URL response = " + url_response)
-            return json.loads('[]')
+        print("ERROR: Unable to process JSON response: " + str(error))
+        print("ERROR: Status code = %s"%(url_response.status_code))
+        print("ERROR: Reason = %s"%(url_response.reason))
+        if verbose:
+            print("ERROR: Query = " + str(query_dict))
+        return None
     except Exception as error:
         print("ERROR: Unable to process JSON response: " + str(error))
+        print("ERROR: Status code = %s"%(url_response.status_code))
+        print("ERROR: Reason = %s"%(url_response.reason))
         if verbose:
-            print("ERROR: JSON = " + url_response)
-        return json.loads('[]')
+            print("ERROR: Query = " + str(query_dict))
+        return None
 
     # Return the JSON data
     return json_data
@@ -77,114 +100,62 @@ def initHTTP():
         getattr(ssl, '_create_unverified_context', None)): 
         ssl._create_default_https_context = ssl._create_unverified_context
 
-def generateQuery(field, value):
-    query_str = "{ \"filters\": { \"op\":\"contains\", \"content\": { \"field\":\"%s\", \"value\":\"%s\" } }, \"facets\":\"repertoire_id\" }"%(field, value)
-    return query_str
+def generateRearrangementQuery(repertoire_id, query_dict):
+    # Create a new query
+    query_with_repertoire = dict()
+    if "filters" in query_dict:
+        # Add an and clause that contains the repertoire_id and the original query
+        and_filter_list = []
+        and_filter_list.append({ "op":"=", "content": { "field":"repertoire_id", "value":str(repertoire_id) }})
+        and_filter_list.append(query_dict["filters"])
+    
+        query_with_repertoire = {"filters": { "op":"and", "content": and_filter_list}}
+    else:
+        query_with_repertoire = {"filters": {"op":"=", "content": { "field":"repertoire_id", "value":str(repertoire_id) }}}
+
+    #query_with_repertoire["filters"]["content"].append({ "op":"=", "content": { "field":"repertoire_id", "value":str(repertoire_id) }}, original_filter ] }}
+
+    # Copy any other query info other than filters (e.g. facets, from, size etc)
+    for query_field in query_dict:
+        if not query_field == "filters":
+            query_with_repertoire[query_field] = query_dict[query_field]
+
+    # Return the new response
+    return query_with_repertoire
 
 
-def searchCDR3(url, cdr3_file, cdr3_header, verbose):
+def getRepertoires(repertoire_url, reperotire_dict, output_handle, verbose):
     # Ensure our HTTP set up has been done.
     initHTTP()
     # Get the HTTP header information (in the form of a dictionary)
     header_dict = getHeaderDict()
 
-    # Open the file that contains the list of CDR3s to search
-    try:
-        cdr3_df = pd.read_csv(cdr3_file, sep=None, engine='python', encoding='utf-8-sig')
-    except Exception as err:
-        print("ERROR: Unable to open file %s - %s" % (cdr3_file, err))
-        return False
+    if verbose:
+        print('INFO: Query: ' + str(repertoire_dict))
+    # Perform the query.
+    query_json = processQuery(repertoire_url, repertoire_dict, verbose)
+    if verbose:
+        print('INFO: Query response: ' + str(query_json))
 
-    # Get the CDR3 list from the column header.
-    if not cdr3_header in cdr3_df:
-        print("ERROR: Could not find header %s in file %s" % (cdr3_header, cdr3_file))
-        return False
-        
-    # Build the full URL combining the URL and the entry point.
-    query_url = url
+    # Print out an error if the query failed.
+    if query_json == None:
+        print('ERROR: Query %s failed to %s'%(query_json, repertoire_url))
+        return None
 
-    # Iterate over the CDR3s
-    for index, cdr3_row in cdr3_df.iterrows():
-        if verbose:
-            print("INFO: Looking for CDR3 %s"%(cdr3_row[cdr3_header]))
+    # Check for a correct Info object.
+    if not "Info" in query_json:
+        print("ERROR: Expected to find an 'Info' object, none found")
+        return None
 
-        cdr3_query = generateQuery("junction_aa", cdr3_row[cdr3_header])
+    # Check for a valid Repertoire object
+    if not "Repertoire" in query_json:
+        print("ERROR: Expected to find an 'Repertoire' object, none found")
+        return None
 
-        if verbose:
-            print('INFO: Performing query: ' + str(cdr3_query))
+    repertoire_list = query_json['Repertoire']
 
-        # Perform the query.
-        query_json = processQuery(query_url, header_dict, cdr3_query, verbose)
-        if verbose:
-            print('INFO: Query response: ' + str(query_json))
+    return repertoire_list
 
-        # Print out an error if the query failed.
-        if len(query_json) == 0:
-            print('ERROR: Query %s failed to %s'%(query_json, query_url))
-            continue
-            return False
-
-        # Check for a correct Info object.
-        if not "Info" in query_json:
-            print("ERROR: Expected to find an 'Info' object, none found")
-            return False
-
-        # Check for a correct Facet object.
-        facet_key = "Facet"
-        if not facet_key in query_json:
-            print("ERROR: Expected to find a 'Facet' object, none found")
-            return False
-
-        # Get the Facet array - a facet repsonse looks like this:
-        # 'Facet': [{'count': 71, 'repertoire_id': '5ec449f0ba06faa86ec02506'}]
-        facet_array = query_json[facet_key ]
-        num_responses = len(facet_array)
-        total = 0
-        if num_responses > 0:
-            for repertoire in facet_array:
-                total = total + repertoire["count"]
-            print("%d: Found %d instances of %s in %d repertoires"%
-                  (index, total, cdr3_row[cdr3_header], num_responses),flush=True)
-
-            #print("    Details: %s (%s %s), MHC = %s,%s (%s), Epitope = %s (%s,%s)"%(
-            #      cdr3_row["Gene"], cdr3_row["V"], cdr3_row["J"],
-            #      cdr3_row["MHC A"], cdr3_row["MHC B"], cdr3_row["MHC class"],
-            #      cdr3_row["Epitope"], cdr3_row["Epitope gene"], cdr3_row["Epitope species"]),
-            #      flush=True)
-        else:
-            print("%d: Found %d instances of %s"% (index, total, cdr3_row[cdr3_header]),flush=True)
-                  
-                   
-        time.sleep(0.5)
-    return True
-
-    if False:
-            # check if facets query
-            if query_dict.get('facets'):
-                response_tag = "Facet"
-
-            if not response_tag in query_json:
-                print("ERROR: Expected to find a '" + response_tag +"' object, none found")
-                return 1
-        
-            query_response_array = query_json[response_tag]
-            num_responses = len(query_response_array)
-
-            if not gold_disabled:
-                if gold_results.get(query_name):
-                    if gold_results[query_name].get('records'):
-                        if num_responses != int(gold_results[query_name]['records']):
-                            print("ERROR: Expected " + str(gold_results[query_name]['records']) + " != " + str(num_responses) + " records")
-                            return 1
-                    else:
-                        print('WARNING: No expected records specified for ' + query_name)
-                else:
-                    print('WARNING: No gold expectation for ' + query_name)
-
-            print("INFO: Received " + str(num_responses) + " " + response_tag + "s from query")
-            print('PASS: Query file ' + query_file + ' to ' + query_url + ' OK')
-
-    return 0
 
 def getArguments():
     # Set up the command line parser
@@ -201,19 +172,43 @@ def getArguments():
     parser.add_argument("rearrangement_search_json")
     # Repertoire entry point to use - should be the normal AIRR reptetoire
     parser.add_argument(
-        "--repertoire-api",
+        "--repertoire_api",
         dest="repertoire_api",
         default="/airr/v1/repertoire",
         help="The repertoire API entry point. Defaults to '/airr/v1/repertoire'/")
     # Rearragement entry point to use - should be the normal AIRR rearrangements or 
     # the iR Plus Stats.
     parser.add_argument(
-        "--rearrangement-api",
-        dest="rerrangement_api",
+        "--rearrangement_api",
+        dest="rearrangement_api",
         default="/airr/v1/rearrangement",
         help="The Rearrangement API entry point. Defaults to '/airr/v1/rearrangement'/"
     )
+    # Field file
+    parser.add_argument(
+        "--field_file",
+        dest="field_file",
+        default=None,
+        help="File that contains a list of AIRR fields in dot notation (subject.subject_id). These fields are output in every repertoire query output in a 'Repertoire' object. If no file is provided then an empty repertoire object is created."
+    )
+    # Output file
+    parser.add_argument(
+        "--output_file",
+        dest="output_file",
+        default=None,
+        help="The output file to use. If none supplied, uses stdout."
+    )
 
+    # Choose a time delay between repertoire queries. This is so we can be nice
+    # to the service and not inundate it with queries. Note the service will
+    # reject queries if we go too fast, so this may or may not be needed.
+    parser.add_argument(
+        "--service_delay",
+        dest="service_delay",
+        default=0.0,
+        type=float,
+        help="The service delay to use between rearrangement queries. If we go too fast the serive may reject queries with an error. Default = 0.0"
+    )
     # Flag to determine if AIRR Rearrangement or iR+ Stats API should be used.
     parser.add_argument(
         "-s",
@@ -235,6 +230,27 @@ if __name__ == "__main__":
     # Get the command line arguments.
     options = getArguments()
 
+    # Get the output file handle
+    if options.output_file == None:
+        output_handle = sys.stdout
+    else:
+        try:
+            output_handle = open(options.output_file, "w")
+        except Exception as err:
+            print("ERROR: Unable to open output file %s - %s" % (options.output_file, err))
+            sys.exit(1)
+
+    # Read in the repertoire field file
+    if options.field_file is None:
+        repertoire_field_df = pd.DataFrame([]) 
+    else:
+        try:
+            repertoire_field_df = pd.read_csv(options.field_file, sep='\t',
+                                        engine='python', encoding='utf-8-sig')
+        except Exception as err:
+            print("ERROR: Unable to open file %s - %s" % (options.repository_url_file, err))
+            sys.exit(1)
+
     # Read in the repository file
     try:
         repository_df = pd.read_csv(options.repository_url_file, sep='\t',
@@ -245,22 +261,29 @@ if __name__ == "__main__":
 
     # Open the Repertoire JSON query file.
     with open(options.repertoire_search_json) as f:
-        repertoire_json = json.load(f)
-    print("Repertoire query = %s"%(repertoire_json))
+        repertoire_dict = json.load(f)
+        repertoire_json = str(repertoire_dict)
+    if options.verbose:
+        print("Repertoire query = %s"%(repertoire_json))
 
     # Open the Rearrangement JSON query file.
     with open(options.rearrangement_search_json) as f:
-        rearrangement_json = json.load(f)
-    print("Rearrangement query = %s"%(rearrangement_json))
+        rearrangement_dict = json.load(f)
+        rearrangement_json = str(rearrangement_dict)
+    if options.verbose:
+        print("Rearrangement query = %s"%(rearrangement_json))
 
     for index, row in repository_df.iterrows():
-        print("Row %d: %s"% (index, row['URL']+options.repertoire_api))
-        #repertoires = getRepertoires(row['URL']+options.repertoire_api,
+        if options.verbose:
+            print("Row %d: %s"% (index, row['URL']+options.repertoire_api))
+        repertoires = getRepertoires(row['URL']+options.repertoire_api,
+                repertoire_dict, output_handle, options.verbose)
 
-    # Perform the query analysis, gives us back a dictionary.
-    #success = searchCDR3(options.url, options.cdr3_file,
-    #                     options.column_header, options.verbose)
-    # Return success if successful
+        if not repertoires == None:
+            performRearrangementQuery(row['URL']+options.rearrangement_api,
+                    repertoires, rearrangement_dict, repertoire_field_df, output_handle,
+                    options.service_delay, options.verbose)
+
     #if not success:
     #    sys.exit(1)
 
