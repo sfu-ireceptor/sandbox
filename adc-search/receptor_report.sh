f39a0e83fe97e9b8f77e731b446ff7078ba5e8cb
bcorrie <bdcorrie@gmail.com>
2024-07-24 16:44:23 +0000

    Add download of ADC data. Clean up some other odds and ends

--- adc-search/receptor_report.sh
+++ adc-search/receptor_report.sh
@@ -44,12 +44,25 @@ echo '] }, "facets":"repertoire_id"}' >> $JSON_QUERY_FILE
 
 # Search the ADC for instances of said receptor
 
-python3 ${SCRIPT_DIR}/adc-search.py ${REPOSITORY_TSV} ${REPERTOIRE_QUERY_JSON} $JSON_QUERY_FILE --service_delay=0.2 --output_file=$OUTPUT_DIR/all-$CDR3.json --field_file=${REPERTOIRE_FIELD_TSV} --verbose > $OUTPUT_DIR/all-$CDR3.out
+python3 ${SCRIPT_DIR}/adc-search.py ${REPOSITORY_TSV} ${REPERTOIRE_QUERY_JSON} $JSON_QUERY_FILE --service_delay=0.2 --output_file=$OUTPUT_DIR/count-$CDR3.json --field_file=${REPERTOIRE_FIELD_TSV} --verbose > $OUTPUT_DIR/count-$CDR3.out
+
+# Generate the filter to download the receptor chain
+
+echo '{ "filters": { "op" : "and", "content" : [' > $JSON_QUERY_FILE
+echo "{ \"op\":\"=\", \"content\": { \"field\":\"junction_aa\", \"value\":\"${CDR3}\"}}," >> $JSON_QUERY_FILE
+echo "{ \"op\":\"=\", \"content\": { \"field\":\"v_gene\", \"value\":\"${VGENE}\"}}," >> $JSON_QUERY_FILE
+echo "{ \"op\":\"=\", \"content\": { \"field\":\"j_gene\", \"value\":\"${JGENE}\"}}" >> $JSON_QUERY_FILE
+echo '] }, "format":"tsv"}' >> $JSON_QUERY_FILE
+
+# Download the sequences for the receptor
+
+python3 ${SCRIPT_DIR}/adc-search.py --verbose ${REPOSITORY_TSV} ${REPERTOIRE_QUERY_JSON} $JSON_QUERY_FILE --output_format=TSV --output_file=$OUTPUT_DIR/download-$CDR3.tsv --output_dir=${OUTPUT_DIR}/ --field_file=${REPERTOIRE_FIELD_TSV} > $OUTPUT_DIR/download-$CDR3.out
+
 
 # Generate a report for the repertoires where the receptor was found
 
-COUNT=`cat $OUTPUT_DIR/all-$CDR3.json | jq '[.[].results[].Facet[].count]| add | if . > 0 then . else 0 end'`
-REPERTOIRES=`cat $OUTPUT_DIR/all-$CDR3.json | jq ' [.[].results[].Facet[].count]| length'`
+COUNT=`cat $OUTPUT_DIR/count-$CDR3.json | jq '[.[].results[].Facet[].count]| add | if . > 0 then . else 0 end'`
+REPERTOIRES=`cat $OUTPUT_DIR/count-$CDR3.json | jq ' [.[].results[].Facet[].count]| length'`
 
 echo "" >> $REPORT_FILE
 echo "The Receptor $CDR3/$VGENE/$JGENE was found $COUNT times in the ADC" >> $REPORT_FILE
@@ -61,18 +74,18 @@ echo "" >> $REPORT_FILE
 #echo "The Receptor $CDR3/$VGENE/$JGENE was found in the following ADC repertoires" >> $REPORT_FILE
 #echo "" >> $REPORT_FILE
 #
-#cat $OUTPUT_DIR/all-$1.json | jq ' [ .[].results[] | if .Facet[0].count > 0 then {Repertoire: .Repertoire, count: .Facet[0].count} else empty end ]' >> $REPORT_FILE
+#cat $OUTPUT_DIR/count-$1.json | jq ' [ .[].results[] | if .Facet[0].count > 0 then {Repertoire: .Repertoire, count: .Facet[0].count} else empty end ]' >> $REPORT_FILE
 
 # Generate a list of all Receptors in IEDB that contain the CDR3
 
 curl -s https://query-api.iedb.org/tcr_search?chain1_cdr3_seq=like.*$CDR3* > $OUTPUT_DIR/iedb-tcr-chain1-$CDR3.json
-echo "Found TCR receptors specific to $CDR3 in IEDB:" >> $REPORT_FILE
+echo "Found TCR receptors that contain $CDR3 in IEDB:" >> $REPORT_FILE
 cat $OUTPUT_DIR/iedb-tcr-chain1-$CDR3.json | jq '[ .[] | { receptor_group_iri: .receptor_group_iri, receptor_chain1_types: .receptor_chain1_types, chain1_cdr3_seq: .chain1_cdr3_seq,  receptor_chain2_types: .receptor_chain2_types, chain2_cdr3_seq: .chain2_cdr3_seq, antigens: .curated_source_antigens, epitope_iris: .structure_iris, epitopes: .structure_descriptions, disease_names: .disease_names} ]' >> $REPORT_FILE
 
 curl -s https://query-api.iedb.org/tcr_search?chain2_cdr3_seq=like.*$CDR3* > $OUTPUT_DIR/iedb-tcr-chain2-$CDR3.json
 cat $OUTPUT_DIR/iedb-tcr-chain2-$CDR3.json | jq '[ .[] | { receptor_group_iri: .receptor_group_iri, receptor_chain1_types: .receptor_chain1_types, chain1_cdr3_seq: .chain1_cdr3_seq,  receptor_chain2_types: .receptor_chain2_types, chain2_cdr3_seq: .chain2_cdr3_seq, antigens: .curated_source_antigens, epitope_iris: .structure_iris, epitopes: .structure_descriptions, disease_names: .disease_names} ]' >> $REPORT_FILE
 
-echo "Found BCR receptors specific to $CDR3 in IEDB:" >> $REPORT_FILE
+echo "Found BCR receptors that contain $CDR3 in IEDB:" >> $REPORT_FILE
 curl -s https://query-api.iedb.org/bcr_search?chain1_cdr3_seq=like.*$CDR3* > $OUTPUT_DIR/iedb-bcr-chain1-$CDR3.json
 cat $OUTPUT_DIR/iedb-bcr-chain1-$CDR3.json | jq '[ .[] | { receptor_group_iri: .receptor_group_iri, receptor_chain1_types: .receptor_chain1_types, chain1_cdr3_seq: .chain1_cdr3_seq,  receptor_chain2_types: .receptor_chain2_types, chain2_cdr3_seq: .chain2_cdr3_seq, antigens: .curated_source_antigens, epitope_iris: .structure_iris, epitopes: .structure_descriptions, disease_names: .disease_names} ]' >> $REPORT_FILE
 curl -s https://query-api.iedb.org/bcr_search?chain2_cdr3_seq=like.*$CDR3* > $OUTPUT_DIR/iedb-bcr-chain2-$CDR3.json
@@ -87,7 +100,7 @@ curl -s "https://query-api.iedb.org/epitope_search?linear_sequence=eq.$EPITOPE" 
 echo "" >> $REPORT_FILE
 echo "Epitope data for $EPITOPE was found in IEDB" >> $REPORT_FILE
 echo "" >> $REPORT_FILE
-cat $OUTPUT_DIR/iedb-epitope-$EPITOPE.json | jq '. | [ { linear_sequence: .[].linear_sequence, structure_iri: .[].structure_iri, antigens: .[].curated_source_antigens, receptor_group_iris: .[].receptor_group_iris} ]' >> $REPORT_FILE
+cat $OUTPUT_DIR/iedb-epitope-$EPITOPE.json | jq '. | [ { linear_sequence: .[].linear_sequence, structure_type: .[].structure_type, structure_iri: .[].structure_iri, host_organism_names: .[].host_organism_names, mhc_allele_names: .[].mhc_allele_names, disease_names: .[].disease_names, r_object_source_molecule_names: .[].r_object_source_molecule_names, receptor_group_iris: .[].receptor_group_iris} ]' >> $REPORT_FILE
 
 
 echo -n "Done query at: " >> $REPORT_FILE
