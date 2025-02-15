995c797dc0038ce562fea89b979fd5796cf03608
bcorrie <bdcorrie@gmail.com>
2025-02-15 17:47:16 +0000

    Changed so generates array of strings for each IEDB object

--- adc-search/hits.sh
+++ adc-search/hits.sh
@@ -26,57 +26,89 @@ tmp_file=$(mktemp)
 
 # Loop over the directories we found that contain valid receptor hits.
 while IFS= read -r dir; do
+    echo "Processing $dir"
+    # Look up the IEDB recepetor
+    receptor_string=$(basename $dir)
+    IFS='_' read -r junction_aa v_gene j_gene <<< "$receptor_string"
+    #echo $junction_aa 
+    #echo $v_gene 
+    #echo $j_gene 
+    cdr3_aa="${junction_aa:1:-1}"
+    #echo $cdr3_aa 
+    #curl -s https://query-api.iedb.org/tcr_search?chain1_cdr3_seq=like.$junction_aa | \
+		#jq -r 'if length > 0 then [.[] | [.receptor_group_iri] else empty end'
+    #iedb_receptor_iri="$(curl -s https://query-api.iedb.org/tcr_search?chain2_cdr3_seq=like.*$cdr3_aa*| \
+    #	jq 'if length > 0 then [.[] | .receptor_group_iri ] else empty end' | \
+    #	tr -d '\n' | tr -d ' ')"
+    #query_response_str=$(curl -s https://query-api.iedb.org/tcr_search?chain2_cdr3_seq=like.*$cdr3_aa*\&\&select=chain2_cdr3_seq,receptor_group_iri,curated_source_antigens,structure_iris,tcr_export\(chain_2__curated_v_gene,chain_2__curated_j_gene\) | jq -r 'if length > 0 then .[] | "\(.receptor_group_iri)\t\(.chain2_cdr3_seq)\t \(.tcr_export[0].chain_2__curated_v_gene)\t\(.tcr_export[0].chain_2__curated_j_gene)" else empty end')
+    query_response_str=$(curl -s https://query-api.iedb.org/tcr_search?chain2_cdr3_seq=like.*$cdr3_aa*\&\&select=chain2_cdr3_seq,receptor_group_iri,curated_source_antigens,structure_iris,tcr_export\(chain_2__curated_v_gene,chain_2__curated_j_gene,chain_2__calculated_v_gene,chain_2__calculated_j_gene\) )
+    #echo $query_response_str 
+    iedb_receptor_iri=$(echo $query_response_str | \
+        jq --arg v $v_gene --arg j $j_gene '[ .[] | select(.tcr_export[] | (if .chain_2__calculated_v_gene != null then .chain_2__calculated_v_gene else .chain_2__curated_v_gene end | split("*") | .[0] == $v) and (if .chain_2__calculated_j_gene != null then .chain_2__calculated_j_gene else .chain_2__curated_j_gene end | split("*") | .[0] == $j)) ] | [.[].receptor_group_iri ] | unique' | \
+	tr -d '\n' | tr -d ' ')
+    #echo $iedb_receptor_iri
+    iedb_organism_iri="$(echo $query_response_str | \
+        jq --arg v $v_gene --arg j $j_gene '[ .[] | select(.tcr_export[] | (if .chain_2__calculated_v_gene != null then .chain_2__calculated_v_gene else .chain_2__curated_v_gene end | split("*") | .[0] == $v) and (if .chain_2__calculated_j_gene != null then .chain_2__calculated_j_gene else .chain_2__curated_j_gene end | split("*") | .[0] == $j)) ] | [.[].curated_source_antigens[].source_organism_iri ] | unique' | \
+        tr -d '\n' | tr -d ' ' )"
+    #echo $iedb_organism_iri
+    iedb_antigen_iri="$(echo $query_response_str | \
+        jq --arg v $v_gene --arg j $j_gene '[ .[] | select(.tcr_export[] | (if .chain_2__calculated_v_gene != null then .chain_2__calculated_v_gene else .chain_2__curated_v_gene end | split("*") | .[0] == $v) and (if .chain_2__calculated_j_gene != null then .chain_2__calculated_j_gene else .chain_2__curated_j_gene end | split("*") | .[0] == $j)) ] | [.[].curated_source_antigens[].iri ] | unique' | \
+        tr -d '\n' | tr -d ' ' )"
+    #echo $iedb_antigen_iri
+    iedb_epitope_iri="$(echo $query_response_str | \
+        jq --arg v $v_gene --arg j $j_gene '[ .[] | select(.tcr_export[] | (if .chain_2__calculated_v_gene != null then .chain_2__calculated_v_gene else .chain_2__curated_v_gene end | split("*") | .[0] == $v) and (if .chain_2__calculated_j_gene != null then .chain_2__calculated_j_gene else .chain_2__curated_j_gene end | split("*") | .[0] == $j)) ] | [.[].structure_iris[] ] | unique' | \
+        tr -d '\n' | tr -d ' ' )"
+    #echo $iedb_epitope_iri
+    #query_response_str=$(curl -s https://query-api.iedb.org/tcr_search?chain2_cdr3_seq=like.*$cdr3_aa*)
+    #iedb_receptor_iri="$(echo $query_response_str | \
+        #jq 'if length > 0 then [.[] | .receptor_group_iri] else empty end' | \
+	#tr -d '\n' | tr -d ' ')"
+    #iedb_antigen_info="$(echo $query_response_str | \
+    #    jq 'if length > 0 then [.[] | .curated_source_antigens] else empty end' | \
+    #    tr -d '\n' | tr -s ' ' )"
+    #iedb_epitope_iri="$(echo $query_response_str | \
+    #    jq 'if length > 0 then [.[] | .structure_iris] else empty end' | \
+    #    tr -d '\n' | tr -s ' ' )"
+
+    for file in $dir/rearrangement*.tsv; do
+	echo "    Processing $(basename $file)"
 	# Get the number of rearrangements (less the header)
-	line_count=$(tail +2 $dir/rearrangement*.tsv | wc -l)
+	line_count=$(tail +2 $file | wc -l)
 
 	# Get the column where the reperotire_id is stored.
-	repertoire_column=$(awk -F'\t' -v label="repertoire_id" 'NR==1 {for (i=1; i<=NF; i++) if ($i == label) print i; exit}' $dir/rearrangement*.tsv)
+	repertoire_column=$(awk -F'\t' -v label="repertoire_id" 'NR==1 {for (i=1; i<=NF; i++) if ($i == label) print i; exit}' $file)
+	sequence_column=$(awk -F'\t' -v label="sequence_id" 'NR==1 {for (i=1; i<=NF; i++) if ($i == label) print i; exit}' $file)
+	v_column=$(awk -F'\t' -v label="v_call" 'NR==1 {for (i=1; i<=NF; i++) if ($i == label) print i; exit}' $file)
+	j_column=$(awk -F'\t' -v label="j_call" 'NR==1 {for (i=1; i<=NF; i++) if ($i == label) print i; exit}' $file)
+	junction_column=$(awk -F'\t' -v label="junction_aa" 'NR==1 {for (i=1; i<=NF; i++) if ($i == label) print i; exit}' $file)
 	
 	# Extract the list of unique repertoires into a tmp file and count them 
-	tail +2 $dir/rearrangement*.tsv | cut -f $repertoire_column | sort -u > $tmp_file
+	tail +2 $file | cut -f $repertoire_column | sort -u > $tmp_file
 	repertoire_count=$(cat $tmp_file | wc -l)
 
 	# Add these repertoires to the global list of repertoires.
 	cat $tmp_file >> $repertoire_tmp_file
 
-	# Look up the IEDB recepetor
-	receptor_string=$(basename $dir)
-	IFS='_' read -r junction_aa v_gene j_gene <<< "$receptor_string"
-	cdr3_aa="${junction_aa:1:-1}"
-	#curl -s https://query-api.iedb.org/tcr_search?chain1_cdr3_seq=like.$junction_aa | \
-		#jq -r 'if length > 0 then [.[] | [.receptor_group_iri] else empty end'
-	#iedb_receptor_iri="$(curl -s https://query-api.iedb.org/tcr_search?chain2_cdr3_seq=like.*$cdr3_aa*| \
-	#	jq 'if length > 0 then [.[] | .receptor_group_iri ] else empty end' | \
-	#	tr -d '\n' | tr -d ' ')"
-	query_response_str=$(curl -s https://query-api.iedb.org/tcr_search?chain2_cdr3_seq=like.*$cdr3_aa*)
-	iedb_receptor_iri="$(echo $query_response_str | \
-		jq 'if length > 0 then [.[] | .receptor_group_iri] else empty end' | \
-		tr -d '\n' | tr -d ' ')"
-	iedb_antigen_info="$(echo $query_response_str | \
-                jq 'if length > 0 then [.[] | .curated_source_antigens] else empty end' | \
-                tr -d '\n' | tr -s ' ' )"
-	iedb_epitope_iri="$(echo $query_response_str | \
-                jq 'if length > 0 then [.[] | .structure_iris] else empty end' | \
-                tr -d '\n' | tr -s ' ' )"
-
-	#curl -s https://query-api.iedb.org/tcr_search?chain2_cdr3_seq=like.*ASSDSAGELF*\&\&select=chain2_cdr3_seq,receptor_group_iri,tcr_export\(chain_2__curated_v_gene,chain_2__curated_j_gene\) | jq -r 'if length > 0 then .[] | "\(.receptor_group_iri)\t\(.chain2_cdr3_seq)\t \(.tcr_export[0].chain_2__curated_v_gene)\t\(.tcr_export[0].chain_2__curated_j_gene)" else empty end'
-
-
 	# Generate the rearrangements annotated with epitope
-	awk -F'\t' -v iedb_receptor_iri=$iedb_receptor_iri -v iedb_antigen_info="$iedb_antigen_info" -v iedb_epitope_iri="$iedb_epitope_iri" 'NR>1 {printf("%s\t%s\t%s\t%s\t%s\t%s\t%s\n",$1,$11,$14,$22,iedb_receptor_iri,iedb_epitope_iri, iedb_antigen_info);}' $dir/rearrangement*.tsv >> $output_file
+	awk -F'\t' -v sequence_column=$sequence_column -v repertoire_column=$repertoire_column \
+		-v v_column=$v_column -v j_column=$j_column -v junction_column=$junction_column \
+		-v iedb_receptor_iri=$iedb_receptor_iri -v iedb_antigen_iri="$iedb_antigen_iri" \
+		-v iedb_epitope_iri="$iedb_epitope_iri" -v iedb_organism_iri="$iedb_organism_iri"\
+		'NR>1 {printf("%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\n",$sequence_column,$repertoire_column,$v_column,$j_column,$junction_column,iedb_receptor_iri,iedb_epitope_iri, iedb_antigen_iri, iedb_organism_iri);}' $file >> $output_file
 
 	# Print out some reporting
-	echo "Processing $dir"
-	echo "    Number of Rearrangements with match for $(basename $dir) = $line_count"
-	echo "    Number of Repertoires with match for $(basename $dir) = $repertoire_count"
-	echo "    Receptor IRI = $iedb_receptor_iri"
-	echo "    Epitope IRI = $iedb_epitope_iri"
-	echo "    Antigen info = ${iedb_antigen_info:0:50}..."
+	echo "        Number of Rearrangements with match for $(basename $dir) = $line_count"
+	echo "        Number of Repertoires with match for $(basename $dir) = $repertoire_count"
+	echo "        Receptor IRI = $iedb_receptor_iri"
+	echo "        Epitope IRI = $iedb_epitope_iri"
+	echo "        Antigen info = $iedb_antigen_iri"
+	echo "        Organism info = $iedb_organism_iri"
 
 	# Keep track of the totals
 	total_repertoires=$((total_repertoires + repertoire_count))
 	total_rearrangements=$((total_rearrangements + line_count))
 	total_receptors=$((total_receptors + 1))
+    done
 
 # The directories we are interested in are not the misses. We traverse all other
 # subdirectories (should be hits) and process any directory that starts with C
