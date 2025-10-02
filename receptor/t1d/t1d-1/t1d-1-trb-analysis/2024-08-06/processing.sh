157d3e75f5d2dbfb508d9c2a13e34a674a340150
bcorrie <bdcorrie@gmail.com>
2025-10-02 21:43:04 +0000

    Processing file for IEDB TRB

---
+++ receptor/t1d/t1d-1/t1d-1-trb-analysis/2024-08-06/processing.sh
@@ -0,0 +1,46 @@
+# Run the IEDB receptor search (TAKES A VERY LONG TIME - DAYS)
+nohup bash run-t1d-1.sh > t1d-1-2024-12-08.out
+
+# Annotate each sequence in the analysis with its epitope (TAKES A WHILE - HOURS)
+#nohup bash ../../../hits.sh hits-sequence-2025-02-15.tsv > hits-2025-02-15.out
+nohup bash ../../../hits.sh hits-sequence-2025-06-30.tsv > hits-2025-06-30.out
+
+# Extract the unique receptors, epitopes, antigens, and organisms
+bash ../../../unique_extract.sh hits-sequence-2025-06-30.tsv 6 > receptors-2025-06-30.tsv
+bash ../../../unique_extract.sh hits-sequence-2025-06-30.tsv 7 > epitopes-2025-06-30.tsv
+bash ../../../unique_extract.sh hits-sequence-2025-06-30.tsv 8 > antigens-2025-06-30.tsv
+bash ../../../unique_extract.sh hits-sequence-2025-06-30.tsv 9 > organisms-2025-06-30.tsv
+
+# Extract the repertoire IDs from the Seay study across different conditions
+curl -s -d @Seay-TRB-Healthy.json https://t1d-1.ireceptor.org/airr/v1/repertoire | jq -r '.Repertoire[].repertoire_id' > Seay-TRB-Healthy-Repertoire.tsv
+curl -s -d @Seay-TRB-T1D.json https://t1d-1.ireceptor.org/airr/v1/repertoire | jq -r '.Repertoire[].repertoire_id' > Seay-TRB-T1D-Repertoire.tsv
+curl -s -d @Seay-TRB-T2D.json https://t1d-1.ireceptor.org/airr/v1/repertoire | jq -r '.Repertoire[].repertoire_id' > Seay-TRB-T2D-Repertoire.tsv
+curl -s -d @Seay-TRB-DM.json https://t1d-1.ireceptor.org/airr/v1/repertoire | jq -r '.Repertoire[].repertoire_id' > Seay-TRB-DM-Repertoire.tsv
+
+# Extract the sequences from the Seay study across different conditions
+grep -f Seay-TRB-DM-Repertoire.tsv hits-sequence-2025-02-18.tsv > hits-Seay-TRB-DM-2025-02-18.tsv
+grep -f Seay-TRB-Healthy-Repertoire.tsv hits-sequence-2025-02-18.tsv > hits-Seay-TRB-Healthy-2025-02-18.tsv
+grep -f Seay-TRB-T2D-Repertoire.tsv hits-sequence-2025-02-18.tsv > hits-Seay-TRB-T2D-2025-02-18.tsv
+grep -f Seay-TRB-T1D-Repertoire.tsv hits-sequence-2025-02-18.tsv > hits-Seay-TRB-T1D-2025-02-18.tsv
+
+# Could the receptor, epitope, antigen, and species values across conditions
+bash unique_extract.sh hits-Seay-TRB-DM-2025-02-18.tsv 6 | wc -l
+bash unique_extract.sh hits-Seay-TRB-DM-2025-02-18.tsv 7 | wc -l
+bash unique_extract.sh hits-Seay-TRB-DM-2025-02-18.tsv 8 | wc -l
+bash unique_extract.sh hits-Seay-TRB-DM-2025-02-18.tsv 9 | wc -l
+
+bash unique_extract.sh hits-Seay-TRB-T1D-2025-02-18.tsv 6 | wc -l
+bash unique_extract.sh hits-Seay-TRB-T1D-2025-02-18.tsv 7 | wc -l
+bash unique_extract.sh hits-Seay-TRB-T1D-2025-02-18.tsv 8 | wc -l
+bash unique_extract.sh hits-Seay-TRB-T1D-2025-02-18.tsv 9 | wc -l
+
+bash unique_extract.sh hits-Seay-TRB-T2D-2025-02-18.tsv 6 | wc -l
+bash unique_extract.sh hits-Seay-TRB-T2D-2025-02-18.tsv 7 | wc -l
+bash unique_extract.sh hits-Seay-TRB-T2D-2025-02-18.tsv 8 | wc -l
+bash unique_extract.sh hits-Seay-TRB-T2D-2025-02-18.tsv 9 | wc -l
+
+bash unique_extract.sh hits-Seay-TRB-Healthy-2025-02-18.tsv 6 | wc -l
+bash unique_extract.sh hits-Seay-TRB-Healthy-2025-02-18.tsv 7 | wc -l
+bash unique_extract.sh hits-Seay-TRB-Healthy-2025-02-18.tsv 8 | wc -l
+bash unique_extract.sh hits-Seay-TRB-Healthy-2025-02-18.tsv 9 | wc -l
+
