e57b79af08459a7bd9c72b9b67dfa6840ad6ac53
bcorrie <bdcorrie@gmail.com>
2025-10-07 22:21:35 +0000

    Update processing

--- receptor/t1d/t1d-3/t1d-3-trb-analysis/2024-08-06/processing.sh
+++ receptor/t1d/t1d-3/t1d-3-trb-analysis/2024-08-06/processing.sh
@@ -3,21 +3,11 @@ nohup bash run_t1d-3.sh > t1d-3-2025-03-22.out
 
 # Generate sequences with hits (hours)
 nohup bash $IR_SANDBOX/adc-search/hits-trb.sh hits-sequence-trb-2024-08-06-2025-10-03.tsv > hits-2024-08-06-2025-10-03.out &
-
-# Count number of sequences
-$ wc -l hits-sequence-trb-2024-08-06-2025-10-03.tsv
-# 874173 hits-sequence-trb-2024-08-06-2025-10-03.tsv
+head -1 ../../../t1d-1/t1d-1-trb-analysis/2024-08-06/hits-sequence-trb-header-2024-08-06-2025-10-02.tsv > hits-sequence-trb-header-2024-08-06-2025-10-03.tsv
+cat hits-sequence-trb-2024-08-06-2025-10-03.tsv >> hits-sequence-trb-header-2024-08-06-2025-10-03.tsv
+wc -l hits-sequence-trb-*
+   874173 hits-sequence-trb-2024-08-06-2025-10-03.tsv
+   874174 hits-sequence-trb-header-2024-08-06-2025-10-03.tsv
 # wc -l hits-sequences-2025-04-02.out
 # 874173 hits-sequences-2025-04-02.out
-
-# Count number of unique repertoires (column 2)
-$ cut -f 2 hits-sequence-trb-2024-08-06-2025-10-03.tsv | sort -u | wc -l
-# 2072
-# cut -f 2 hits-sequences-2025-04-02.out | sort -u | wc -l
-# 2072
-
-# Count number of unique IEDB receptors (column 6)
-bash $IR_SANDBOX/adc-search/unique_extract.sh hits-sequence-trb-2024-08-06-2025-10-03.tsv 6 > receptors-2025-10-06.tsv
-wc -l receptors-2025-10-06.tsv
-# 26864 receptors-2025-10-06.tsv
 
