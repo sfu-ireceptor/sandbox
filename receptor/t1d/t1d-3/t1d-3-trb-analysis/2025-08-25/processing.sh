2f72d6fef5bf0183642424047f58c9213e1a2552
bcorrie <bdcorrie@gmail.com>
2025-10-09 19:26:06 +0000

    Processing changes due to hits-trb.sh fixes

--- receptor/t1d/t1d-3/t1d-3-trb-analysis/2025-08-25/processing.sh
+++ receptor/t1d/t1d-3/t1d-3-trb-analysis/2025-08-25/processing.sh
@@ -2,9 +2,13 @@
 nohup bash run-t1d-3.sh "2025-08-25_trb_additions_*" $IR_SANDBOX/receptor/2025-08-25-TR-IG > t1d-3-2025-10-03.out &
 
 # Generate sequences with hits (hours)
-nohup bash $IR_SANDBOX/adc-search/hits-trb.sh hits-sequence-trb-2025-08-25-2025-10-06.tsv > hits-2025-08-25-2025-10-06.out &
-
-# Count number of sequences
-$ wc -l hits-sequence-trb-2025-08-25-2025-10-06.tsv
-1997 hits-sequence-trb-2025-08-25-2025-10-06.tsv
+nohup bash $IR_SANDBOX/adc-search/hits-trb.sh hits-sequence-trb-2025-08-25-2025-10-09.tsv > hits-2025-08-25-2025-10-09.out
+head -1 hits-sequence-trb-header-2025-08-25-2025-10-06.tsv > hits-sequence-trb-header-2025-08-25-2025-10-09.tsv
+cat hits-sequence-trb-2025-08-25-2025-10-09.tsv >> hits-sequence-trb-header-2025-08-25-2025-10-09.tsv
+wc -l hits-sequence-trb-*.tsv
+   1997 hits-sequence-trb-2025-08-25-2025-10-06.tsv
+   1997 hits-sequence-trb-2025-08-25-2025-10-09.tsv
+   1998 hits-sequence-trb-header-2025-08-25-2025-10-06.tsv
+   1998 hits-sequence-trb-header-2025-08-25-2025-10-09.tsv
+   7990 total
 
