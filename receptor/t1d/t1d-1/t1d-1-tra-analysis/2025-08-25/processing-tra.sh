a1b9d0fd62d5d56a96e86be849d537c4bfb0f035
bcorrie <bdcorrie@gmail.com>
2025-10-09 19:03:02 +0000

    New processing after fixes in hits-tra.sh

--- receptor/t1d/t1d-1/t1d-1-tra-analysis/2025-08-25/processing-tra.sh
+++ receptor/t1d/t1d-1/t1d-1-tra-analysis/2025-08-25/processing-tra.sh
@@ -2,11 +2,13 @@
 nohup bash run-t1d-1.sh "2025-08-25_tra_additions_*" /data/src/sandbox/adc-search/receptor/2025-08-25 > 2025-08-27-t1d-1-tra.out
 
 # Annotate each sequence in the analysis with its epitope (TAKES A WHILE - HOURS)
-nohup bash $IR_SANDBOX/adc-search/hits-tra.sh hits-sequence-tra-2025-08-25-2025-10-03.tsv > hits-tra-2025-10-03.out &
-head -1 ../../t1d-1-trb-analysis/2024-08-06/hits-sequence-trb-header-2024-08-06-2025-10-02.tsv > hits-sequence-tra-header-2025-08-25-2025-10-03.tsv
-cat  hits-sequence-tra-2025-08-25-2025-10-03.tsv >> hits-sequence-tra-header-2025-08-25-2025-10-03.tsv
-wc -l *.tsv
-#   42 hits-sequence-tra-2025-08-25-2025-10-03.tsv
-#   43 hits-sequence-tra-header-2025-08-25-2025-10-03.tsv
-
+nohup bash $IR_SANDBOX/adc-search/hits-tra.sh hits-sequence-tra-2025-08-25-2025-10-09.tsv > hits-tra-2025-10-09.out &
+head -1 hits-sequence-tra-header-2025-08-25-2025-10-03.tsv > hits-sequence-tra-header-2025-08-25-2025-10-09.tsv
+cat hits-sequence-tra-2025-08-25-2025-10-09.tsv >> hits-sequence-tra-header-2025-08-25-2025-10-09.tsv
+wc -l hits-sequence-tra-*.tsv
+    42 hits-sequence-tra-2025-08-25-2025-10-03.tsv
+    42 hits-sequence-tra-2025-08-25-2025-10-09.tsv
+    43 hits-sequence-tra-header-2025-08-25-2025-10-03.tsv
+    43 hits-sequence-tra-header-2025-08-25-2025-10-09.tsv
+   170 total
 
