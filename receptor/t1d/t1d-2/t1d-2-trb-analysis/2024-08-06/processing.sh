dd0ff20be004133db2014f114c76ff554db31339
bcorrie <bdcorrie@gmail.com>
2025-10-18 17:45:48 +0000

    Move processing to subdirectory

--- receptor/t1d/t1d-2/t1d-2-trb-analysis/2024-08-06/processing.sh
+++ receptor/t1d/t1d-2/t1d-2-trb-analysis/2024-08-06/processing.sh
@@ -2,9 +2,9 @@
 nohup bash run-t1d-2.sh "trb[0-1]*" /data/src/sandbox/receptor/2024-08-06-TRB > run-t1d-2-2025-10-08.out &
 
 # Generate sequences with hits (hours) - original data with two studies
-nohup bash ../../hits.sh hits-sequences-2025-02-14.out > hits-2025-02-14.out &
+nohup bash $IR_SANDBOX/adc-search/hits-trb.sh hits-sequence-TRB-2024-08-02-2025-10-18.tsv > hits-2025-10-18.out
 
 # Count number of sequences - original data with two studies
-wc -l hits-sequence-2025-02-14.tsv
-6198 hits-sequence-2025-02-14.tsv
+#wc -l hits-sequence-2025-02-14.tsv
+#6198 hits-sequence-2025-02-14.tsv
 
